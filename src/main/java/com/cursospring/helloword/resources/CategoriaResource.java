28a25eed2097be5de0254c6e98bf01fe1937993f
Pedro Cardoso <patchoulypeps@gmail.com>
2019-12-03 15:58:04 -0300

    Adição de REST de teste

---
+++ src/main/java/com/cursospring/helloword/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.cursospring.helloword.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String testar() {
+		return "REST está Ok";
+	}
+}
