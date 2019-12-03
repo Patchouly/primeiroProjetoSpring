c4a6e913247328d80489092c376f4e2a1ec1c88d
Pedro Cardoso <patchoulypeps@gmail.com>
2019-12-03 14:06:10 -0300

    Primeiro commit

---
+++ src/test/java/com/cursospring/helloword/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.helloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
