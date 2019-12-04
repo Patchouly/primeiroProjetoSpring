fe84ea6e3282df6c6381a4bbfffcb78055189211
Pedro Cardoso <patchoulypeps@gmail.com>
2019-12-04 10:39:06 -0300

    Criado operação de instanciação

--- src/main/java/com/cursospring/helloword/CursospringApplication.java
+++ src/main/java/com/cursospring/helloword/CursospringApplication.java
@@ -1,13 +1,34 @@
 package com.cursospring.helloword;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursospring.helloword.domain.Categoria;
+import com.cursospring.helloword.repositories.CategoriaReposiroty;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaReposiroty categoriaReposiroty;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaReposiroty.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 
 }
