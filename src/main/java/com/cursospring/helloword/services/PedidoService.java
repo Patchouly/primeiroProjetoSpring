81e986cf39fe9b72d479d15e29b80ce0e1239d12
Pedro Cardoso <patchoulypeps@gmail.com>
2020-01-07 16:51:50 -0300

    EndPoint /pedidos/{id} online

---
+++ src/main/java/com/cursospring/helloword/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.cursospring.helloword.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursospring.helloword.domain.Pedido;
+import com.cursospring.helloword.repositories.PedidoReposiroty;
+import com.cursospring.helloword.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoReposiroty repo;
+	
+	public Pedido buscaPedido(Integer id) throws ObjectNotFoundException {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow ( 
+				() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Pedido.class.getName()) 
+			);
+	}
+}
