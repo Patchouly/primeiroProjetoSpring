e9718187d9ada553844f425f7bfc80f533bb06f8
Pedro Cardoso <patchoulypeps@gmail.com>
2020-01-10 17:40:17 -0300

    Delete de categoria com DELETE

--- src/main/java/com/cursospring/helloword/services/CategoriaService.java
+++ src/main/java/com/cursospring/helloword/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.cursospring.helloword.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.cursospring.helloword.domain.Categoria;
 import com.cursospring.helloword.repositories.CategoriaReposiroty;
+import com.cursospring.helloword.services.exceptions.DataIntegrityException;
 import com.cursospring.helloword.services.exceptions.ObjectNotFoundException;
 
 
@@ -31,5 +33,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não foi possível excluir uma categoria com produtos!");
+		}
 	}
 }
