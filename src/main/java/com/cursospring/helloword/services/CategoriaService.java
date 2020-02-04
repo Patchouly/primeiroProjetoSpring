a65005a1a096e2a4d67d59cb29d44cd00bcfadac
Pedro Cardoso <patchoulypeps@gmail.com>
2020-02-04 17:02:55 -0300

    Adicionado paginação com parametros opcionais

--- src/main/java/com/cursospring/helloword/services/CategoriaService.java
+++ src/main/java/com/cursospring/helloword/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursospring.helloword.domain.Categoria;
@@ -47,5 +50,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	//Busca paginada
+	public Page<Categoria> findByPage(Integer page, Integer rows, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, rows, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
